2c5054bef7468cd6ba5f8115f0b0e7ab5be71c93
korewayume <korewayume@foxmail.com>
2023-03-09 17:51:42 +0800

    v0.0.9

--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.18
 
 require (
 	github.com/v2pro/plz v0.0.0-20221028024117-e5f9aec5b631
-	go.uber.org/zap v1.23.0
+	go.uber.org/zap v1.24.0
 )
 
 require (
--- logger.go
+++ logger.go
@@ -47,6 +47,8 @@ var Logger *ZLogger
 // MLogger 通过包名直接调用
 var MLogger *ZLogger
 
+var level zap.AtomicLevel
+
 // GoidCallerEncoder caller中增加Goroutine ID
 func GoidCallerEncoder(caller zapcore.EntryCaller, enc zapcore.PrimitiveArrayEncoder) {
 	enc.AppendString(fmt.Sprintf("%d,%d", os.Getpid(), gls.GoID()))
@@ -69,13 +71,15 @@ func init() {
 		ConsoleSeparator: " ",
 	}
 
+	level = zap.NewAtomicLevelAt(zap.InfoLevel)
+
 	// 设置日志级别
 	config := zap.Config{
-		Level:            zap.NewAtomicLevelAt(zap.InfoLevel), // 日志级别
-		Development:      true,                                // 开发模式，堆栈跟踪
-		Encoding:         "console",                           // 输出格式 console 或 json
-		EncoderConfig:    encoderConfig,                       // 编码器配置
-		OutputPaths:      []string{"stdout"},                  // 输出到指定文件 stdout（标准输出，正常颜色） stderr（错误输出，红色）
+		Level:            level,              // 日志级别
+		Development:      true,               // 开发模式，堆栈跟踪
+		Encoding:         "console",          // 输出格式 console 或 json
+		EncoderConfig:    encoderConfig,      // 编码器配置
+		OutputPaths:      []string{"stdout"}, // 输出到指定文件 stdout（标准输出，正常颜色） stderr（错误输出，红色）
 		ErrorOutputPaths: []string{"stderr"},
 	}
 	// 构建日志
@@ -86,6 +90,10 @@ func init() {
 	}
 	Logger = &ZLogger{Logger: *logger}
 	MLogger = &ZLogger{Logger: *logger.WithOptions(zap.AddCallerSkip(1))}
+}
+
+func SetLevel(l zapcore.Level) {
+	level.SetLevel(l)
 }
 
 func Debug(msg string, fields ...zap.Field) {
